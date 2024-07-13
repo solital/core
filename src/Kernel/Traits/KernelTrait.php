e8c3ee6a976ddae4fbe35ae52257add106db089c
Brenno Duarte <brenno.gnr@gmail.com>
2024-07-13 19:40:22 -0300

    Changed version

--- src/Kernel/Traits/KernelTrait.php
+++ src/Kernel/Traits/KernelTrait.php
@@ -12,7 +12,7 @@ trait KernelTrait
     /**
      * That variables must be changed manually
      */
-    const SOLITAL_VERSION   = "4.4.0";
+    const SOLITAL_VERSION   = "4.5.0";
     const SITE_DOC_DOMAIN   = "https://solital.github.io/site/";
 
     /**
